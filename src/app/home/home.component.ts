60956aea96d0a2db76faf429789e5e59b5519cff
sujith <you@example.com>
2024-11-30 12:33:35 +0530

    flatarray for afeef

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { ProductService } from '../product.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  getdata:any
+constructor( private service:ProductService){
+this.getdata=this.service.productdetails['id_123'];
+console.log(this.getdata);
 
+}
 }
