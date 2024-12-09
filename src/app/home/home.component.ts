1381b2da62109b94043e223e3a99915a38bf09a4
Midhlaj1786 <muhmdmidhlaj41@gmail.com>
2024-12-09 13:00:15 +0530

    homepages modified

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,11 @@
  import { Component} from '@angular/core';
  import { HeaderSectionComponent } from "../header-section/header-section.component";
+ import { HomepagesComponent } from "../homepages/homepages.component";
  import {BottompageComponent } from "../bottompage/bottompage.component";
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderSectionComponent,BottompageComponent],
+  imports: [HeaderSectionComponent,HomepagesComponent,BottompageComponent],
   templateUrl: './home.component.html', 
   styleUrls: ['./home.component.scss'] 
 })
